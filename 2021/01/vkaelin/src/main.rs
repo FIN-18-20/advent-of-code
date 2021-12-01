224e53807f6b9bb1ef5d20295c035b31b8c59047
Valentin Kaelin <kalanehd@gmail.com>
2021-12-01 09:37:50 +0100

    feat: add day1 part1

--- 2021/01/vkaelin/src/main.rs
+++ 2021/01/vkaelin/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+    part2();
+}
+
+fn part1() {
+    let filename = "../io/vkaelin.input";
+    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let lines: Vec<_> = content.lines().collect();
+
+    let mut old= 0;
+    let mut counter = 0;
+
+    for (i, line) in lines.iter().enumerate() {
+        let line: i32 = line.parse().expect("Error: line is not a number");
+
+        if i > 0 && line > old {
+          counter += 1;
+        }
+        old = line;
+    }
+
+    println!("Counter: {}", counter);
+}
+
+fn part2() {
+
 }
